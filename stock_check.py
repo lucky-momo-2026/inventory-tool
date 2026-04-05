47a1f412e1fd0ad9af99429b8046c8a060d62643
lucky-momo-2026 <grotta.di.azzura@gmail.com>
2026-04-05 15:37:13 +0900

    在庫切れチェック機能を完成（仕入先表・出力調整）

--- stock_check.py
+++ stock_check.py
@@ -10,7 +10,5 @@ with open('data.csv', 'r', encoding='utf-8') as f:
     #csvの各行を１行ずつ処理
     for row in reader:
         #stock列を整数に変換して「在庫数０」か判定/csvは文字列で読み込まれるためint()が必要
-        print(row["item"], row["stock"])
-
         if int(row['stock']) == 0:
-            print('在庫切れ：', row['item'])  #在庫切れの商品名を表示
+            print('在庫切れ：', row['item'], '/仕入れ先：', row['supplier'])  #在庫切れの商品名と仕入先を表示
