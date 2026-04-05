c9090fa087ecd8497e30939e65aefdf1dcf8b23d
lucky-momo-2026 <grotta.di.azzura@gmail.com>
2026-04-05 16:26:50 +0900

    在庫が少ない商品（5以下）の表示機能を追加

--- stock_check.py
+++ stock_check.py
@@ -2,13 +2,21 @@ import csv
 
 #data.cvsを読み込みモードで開く（UTF-8は日本語対応）
 with open('data.csv', 'r', encoding='utf-8') as f:
-    reader = csv.DictReader(f)  #１行づつ「列名：値」の辞書形式で取得
+    rows = list(csv.DictReader(f))  
 
     print('在庫切れの商品の一覧')  #出力の見出し
     print('------------------')
 
     #csvの各行を１行ずつ処理
-    for row in reader:
+    for row in rows:
         #stock列を整数に変換して「在庫数０」か判定/csvは文字列で読み込まれるためint()が必要
         if int(row['stock']) == 0:
             print('在庫切れ：', row['item'], '/仕入れ先：', row['supplier'])  #在庫切れの商品名と仕入先を表示
+
+    print('在庫が少ない商品の一覧')
+    print('------------------')
+
+    #在庫が少ない商品（５以下）を確認するためにループ
+    for row in rows:
+        if int(row['stock']) <=5:  #sctokは文字列で読み込まれるため数値比較のためintに変換
+            print('在庫少：', row['item'], row['stock'], '/仕入先：', row['supplier'])
